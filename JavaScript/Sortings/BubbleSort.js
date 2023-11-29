73d6e0316b93c6af2a2cac1f7d1282a127cafbbf
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2023-11-29 11:49:01 +0530

    js

---
+++ JavaScript/Sortings/BubbleSort.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')({sigint:true})
+const n = parseInt(prompt("Enter no of eleements: "));
+const arr = [];
+for(i=0;i<n;i++){
+    const a = parseInt(prompt());
+    arr.push(a);
+}
+for(i=0;i<n;i++){
+    for(j=i;j<n;j++){
+        if(arr[i]>arr[j]){
+            const temp = arr[i];
+            arr[i] =arr[j] ;
+            arr[j] =temp;
+        }
+    }
+}
+console.log("Elemets sorted in accending order => ",arr);
